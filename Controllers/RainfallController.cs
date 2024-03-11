62be250379f80317c9ee275ab5f8cd61666d2ee1
ronaldromeo <100099840+ronaldromeo@users.noreply.github.com>
2024-03-11 12:55:16 +0800

    Add files via upload
    Initial commit

---
+++ Controllers/RainfallController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Newtonsoft.Json;
+using RainfallApi.Models;
+
+
+namespace RainfallApi.Controllers
+{
+    [Route("api/[controller]/id")]
+    [ApiController]
+    public class RainfallController : ControllerBase
+    {
+        private readonly ILogger<RainfallController> _logger;
+
+        public RainfallController(ILogger<RainfallController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetRainfall/{stationId}")]
+        public async Task<List<RainfallItem>> GetRainfall(int stationId)
+        {
+            HttpClient client = new HttpClient();
+
+            RainfallReadingModel _result = new RainfallReadingModel();
+            string url = string.Concat("https://environment.data.gov.uk/flood-monitoring/id/stations/", stationId, "/readings?_sorted&_limit=10");
+            HttpResponseMessage response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                string _resultText = await response.Content.ReadAsStringAsync();
+                if (!string.IsNullOrEmpty(_resultText))
+                {
+                    _result = JsonConvert.DeserializeObject<RainfallReadingModel>(_resultText);
+                }
+            }
+            return _result.items;
+        }
+    }
+}
---
+++ Models/RainfallReadingModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+
+namespace RainfallApi.Models
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class RainfallItem
+    {
+        [JsonProperty("@id")]
+        public string id { get; set; }
+        public DateTime dateTime { get; set; }
+        public string measure { get; set; }
+        public double value { get; set; }
+    }
+
+    public class RainfallMetadata
+    {
+        public string publisher { get; set; }
+        public string licence { get; set; }
+        public string documentation { get; set; }
+        public string version { get; set; }
+        public string comment { get; set; }
+        public List<string> hasFormat { get; set; }
+        public int limit { get; set; }
+    }
+
+    public class RainfallReadingModel
+    {
+        [JsonProperty("@context")]
+        public string context { get; set; }
+        public RainfallMetadata meta { get; set; }
+        public List<RainfallItem> items { get; set; }
+    }
+
+
+}
